23e9710e98218e34768a81145a1c3ff628581839
laurinprajescu <laurinprajescu@gmail.com>
2017-08-15 18:25:43 +0100

    single charge for tradesman (not complete)

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,11 @@ from django.shortcuts import render, redirect
 from django.template.context_processors import csrf
 from accounts.forms import UserRegistrationForm, UserLoginForm, TradesmanRegistrationForm
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
+import datetime
+import stripe
 
+stripe.api_key = settings.STRIPE_SECRET
 
 def register(request):
     if request.method == 'POST':
@@ -68,22 +72,33 @@ def tradesman_register(request):
     if request.method == 'POST':
         form = TradesmanRegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
-
-            user = auth.authenticate(email=request.POST.get('email'),
-                                     password=request.POST.get('password1'))
-
-            if user:
-                messages.success(request, "You have successfully registered")
-                return redirect(reverse('profile'))
-
-            else:
-                messages.error(request, "unable to log you in at this time!")
+            try:
+                customer = stripe.Charge.create(
+                    amount=10,
+                    currency="GBP",
+                    description=form.cleaned_data['email'],
+                    card=form.cleaned_data['stripe_id'],
+                )
+                if customer.paid:
+                    form.save()
+                    user = auth.authenticate(email=request.POST.get('email'),
+                                             password=request.POST.get('password1'))
+                    if user:
+                        auth.login(request, user)
+                        messages.success(request, "You have successfully registered")
+                        return redirect(reverse('profile'))
+                    else:
+                        messages.error(request, "unable to log you in at this time!")
+                else:
+                    messages.error(request, "We were unable to take a payment with that card!")
+            except stripe.error.CardError, e:
+                messages.error(request, "Your card was declined!")
 
     else:
+        today = datetime.date.today()
         form = TradesmanRegistrationForm()
 
-    args = {'form': form}
+    args = {'form': form, 'publishable': settings.STRIPE_PUBLISHABLE}
     args.update(csrf(request))
 
-    return render(request, 'register.html', args)
+    return render(request, 'tradesmanregister.html', args)

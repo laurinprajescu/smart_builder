9c4a1624340f58141438348d19a8dd3e0c00591a
laurinprajescu <laurinprajescu@gmail.com>
2017-10-24 19:54:15 +0100

    setup heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn smart_builder.wsgi:application
--- settings/staging.py
+++ settings/staging.py
@@ -12,8 +12,8 @@ DATABASES = {
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_2FKvDfMFtVQSyYQ6jUbwVnm6')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_WisNyom7MSIM6MVcHdj3qP0z')
 
-SITE_URL = 'https://your-heroku-app.herokuapp.com'
-ALLOWED_HOSTS.append('your-heroku-app.herokuapp.com')
+SITE_URL = 'https://smart-builder.herokuapp.com'
+ALLOWED_HOSTS.append('smart-builder.herokuapp.com')
  
 # Log DEBUG information to the console
 LOGGING = {
